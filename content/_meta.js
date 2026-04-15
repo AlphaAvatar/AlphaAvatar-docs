a71a6785a0d3c9caa925c910f37552910604ee1c
Licheng Wang <244267620@qq.com>
2026-04-15 16:18:00 +0800

    update

--- content/_meta.js
+++ content/_meta.js
@@ -1,9 +1,33 @@
 export default {
-  index: 'Home',
-  'getting-started': 'Getting Started',
-  installation: 'Installation',
-  plugins: 'Plugins',
-  channels: 'Channels',
-  examples: 'Examples',
-  papernotes: 'Paper Notes'
+  index: {
+    title: 'Home',
+    type: 'page',
+    theme: {
+      sidebar: false,
+      toc: false,
+      timestamp: false,
+      layout: 'full',
+      pagination: false
+    }
+  },
+  introduction: {
+    title: 'Introduction',
+    type: 'page'
+  },
+  installation: {
+    title: 'Installation',
+    type: 'page'
+  },
+  plugins: {
+    title: 'Plugins',
+    type: 'page'
+  },
+  channels: {
+    title: 'Channels',
+    type: 'page'
+  },
+  examples: {
+    title: 'Examples',
+    type: 'page'
+  }
 }
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import nextra from 'nextra'
 const withNextra = nextra({})
 
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/introduction/getting-started',
+        permanent: false
+      }
+    ]
+  }
 }
 
 export default withNextra(nextConfig)
